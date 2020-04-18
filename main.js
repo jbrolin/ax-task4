e6656e2c2a32d258cebfd5301f81e65484d6d6eb
jbrolin <brolin.jonas@gmail.com>
2020-04-18 13:08:09 +0200

    remove duplicates

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // task4
 
 const run4 = () => {
-  const inputString="Programming"; //document.getElementById("inputString").value;
+  const inputString=document.getElementById("inputString").value;
   
   const tempArray=inputString.split("");
   
@@ -11,15 +11,13 @@ const run4 = () => {
     charObj[tempArray[index]]= !charObj[tempArray[index]] ? 1 : Number(charObj[tempArray[index]])+1;
   }
 
-  // filter object if <=1
-
-  //let newObj = charObj.filter(it => it > 1 );
-  let newObj = charObj.reduce((acc) => {
-    return acc;
-  }, {});
-
+  // remove non duplicates
+  for (const [key, value] of Object.entries(charObj)) {
+    if (Number(value) === 1) {
+      delete charObj[key];
+    }
+  }
   console.log(charObj);
-
-  document.getElementById("outputResult").innerHTML = "output1";
+  document.getElementById("outputResult").innerHTML = JSON.stringify(charObj);
 
 }
